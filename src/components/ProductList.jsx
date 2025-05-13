afcc578d8b237ffb9af200eaccf81d2b562d7b01
Luis F Granados <luisfernando.granados255@comunidadunir.net>
2025-05-13 06:59:56 -0500

    feat: Se añaden nuevas funcionalidades en componentes de listas de productos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,42 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/ProductList.css';
 import { SearchBar } from './SearchBar';
+import { OrderBy } from './OrderBy.jsx';
 
 export const ProductList = ({ libros, search: initialSearch }) => {
-  const [searchQuery, setQuery] = useState(initialSearch || '');
-  const [filteredLibros, setFilteredLibros] = useState(libros);
+    const [searchQuery, setQuery] = useState(initialSearch || '');
+    const [filteredLibros, setFilteredLibros] = useState(libros);
 
-  const handleSearch = (query) => {
-    console.log('Search query en ProductList:', query);
-    setQuery(query);
-  };
+    const handleSearch = (query) => {
+        console.log('Search query en ProductList:', query);
+        setQuery(query);
+    };
 
-  useEffect(() => {
-    const newFilteredLibros = searchQuery
-      ? libros.filter((libro) =>
-          libro.title.toLowerCase().includes(searchQuery.toLowerCase())
-        )
-      : libros;
-    setFilteredLibros(newFilteredLibros);
-  }, [libros, searchQuery]);
+    useEffect(() => {
+        const newFilteredLibros = searchQuery
+            ? libros.filter((libro) => libro.title.toLowerCase().includes(searchQuery.toLowerCase())
+                || libro.author.toLowerCase().includes(searchQuery.toLowerCase())
+                || libro.description.toLowerCase().includes(searchQuery.toLowerCase()))
+            : libros;
+        setFilteredLibros(newFilteredLibros);
+    }, [libros, searchQuery]);
 
-  return (
-    <>
-      <SearchBar onSearch={handleSearch} />
-      <div className="product-list">
-        {filteredLibros.map((libro) => (
-          <div key={libro.id} className="product-card">
-            <img src={libro.image} alt={libro.title} className="product-image" />
-            <h2 className='product-name'>{libro.title}</h2>
-            <p>Autor: {libro.author}</p>
-            <p>Año: {libro.year}</p>
-            <p>{libro.description}</p>
-            <p>Precio: ${libro.price}</p>
-            <button className="add-to-cart-button">Agregar al carrito</button>
-            <button className="view-details">Ver detalles</button>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="search-order-container">
+                <SearchBar onSearch={handleSearch} />
+                <OrderBy />
+            </div>
+            <div className="product-list-container">
+                <div className="product-list">
+                    {filteredLibros.map((libro) => (
+                        <div key={libro.id} className="product-card">
+                            <img src={libro.image} alt={libro.title} className="product-image" />
+                            <h2 className='product-name'>{libro.title}</h2>
+                            <p>Autor: {libro.author}</p>
+                            <p>Año: {libro.year}</p>
+                            <p>{libro.description}</p>
+                            <p>Precio: ${libro.price}</p>
+                            <div className="buttons-container">
+                                <button className="add-to-cart-button">Agregar al carrito</button>
+                                <button className="view-details">Ver detalles</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
