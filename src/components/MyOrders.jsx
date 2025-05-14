66a1301183b531dca807fc7dfc20fc9f85fd9f4e
monicataramuel <100807717+monicataramuel@users.noreply.github.com>
2025-05-14 07:26:24 -0500

    mis pedidos (#6)

---
+++ src/components/MyOrders.jsx
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/MyOrders.css';
+import { SearchBar } from './SearchBar.jsx';
+import { OrderBy } from './OrderBy.jsx';
+import { FilterBy } from './FilterBy.jsx';
+
+export const MyOrders = ({ pedidos, search: initialSearch, order: strDefault }) => {
+    const [searchQuery, setQuery] = useState(initialSearch || '');
+    const [orderBy, setOrderBy] = useState(strDefault || 'default');
+    const [filteredLibros, setFilteredLibros] = useState(pedidos);
+
+    const handleSearch = (query) => {
+        console.log('Search query en MyOrders:', query);
+        setQuery(query);
+    };
+
+    const handleOrder = (valueOrder) => {
+        setOrderBy(valueOrder);
+        console.log('OrderBy en MyOrders:', valueOrder);
+    };
+
+    useEffect(() => {
+        const newFilteredLibros = [...(searchQuery
+            ? pedidos.filter((pedido) => pedido.title.toLowerCase().includes(searchQuery.toLowerCase())
+                || pedido.date.toLowerCase().includes(searchQuery.toLowerCase())
+                || pedido.description.toLowerCase().includes(searchQuery.toLowerCase()))
+            : pedidos)];
+        switch (orderBy) {
+            case 'price-asc':
+                newFilteredLibros.sort((a, b) => a.price - b.price);
+                break;
+            case 'price-desc':
+                newFilteredLibros.sort((a, b) => b.price - a.price);
+                break;
+            case 'year-asc':
+                newFilteredLibros.sort((a, b) => Number(a.year) - Number(b.year));
+                break;
+            case 'year-desc':
+                newFilteredLibros.sort((a, b) => Number(b.year) - Number(a.year));
+                break;
+            default:
+                break;
+        }
+        setFilteredLibros(newFilteredLibros);
+    }, [pedidos, searchQuery, orderBy]);
+
+    return (
+        <>
+            <div className="search-order-container">
+                <SearchBar onSearch={handleSearch} />
+                <OrderBy onOrderBy={handleOrder}/>
+                <FilterBy />
+            </div>
+            <div className="product-list-container">
+             
+                   <table className='table-orders'>
+                      <thead>
+                        <tr>
+                          <th>Fecha de compra</th>
+                          <th>Código de seguimiento</th>
+                          <th>Productos</th>
+                          <th>Estado</th>
+                          <th>Valor Pagado</th>
+                          <th>Seguimiento</th>
+                          <th>Soporte</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                         {filteredLibros.map((pedido) => (
+                            <tr key={pedido.id}>
+                              <td>{pedido.date}</td>
+                              <td>{pedido.tracking}</td>
+                              <td>{pedido.title} <br />{pedido.title2}</td>
+                              <td>{pedido.state}</td>
+                              <td>{pedido.price}</td>
+                              <td>
+                                 <div  style={{ display: 'flex', flexDirection: 'column', gap: '8px', alignItems: 'center' }}>
+                                  {(pedido.state === 'Preparación' || pedido.state === 'Enviado') && (
+                                      <button
+                                        className='boton-actions'
+                                        onClick={() => alert(`Acción para ${pedido.title}`)}
+                                      >
+                                        Seguimiento         
+                                      </button>
+                                    )}
+
+                                   {pedido.state === 'Preparación'  && (
+                                     <>
+                                      <button
+                                        className='boton-actions'
+                                        onClick={() => alert(`Acción para ${pedido.title}`)}
+                                      >
+                                        Modificar        
+                                      </button>
+                                      <button
+                                        className='boton-actions'
+                                        onClick={() => alert(`Acción para ${pedido.title}`)}
+                                      >
+                                        Cancelar        
+                                      </button>
+                                       </>
+                                    )}
+
+                                  {pedido.state === 'Entregado' && (
+                                      <button
+                                        className='boton-actions'
+                                        onClick={() => alert(`Acción para ${pedido.title}`)}
+                                      >
+                                        Volver a comprar        
+                                      </button>
+                                    )}
+                                </div>
+                              </td>
+                              <td>
+                                <div className="buttons-container-detail">
+                                  <button className="add-to-cart-button-detail">Reportar Problemas</button>                                
+                                </div></td>
+                            </tr>
+                          ))}
+                      </tbody>
+                    </table>
+              
+            </div>
+        </>
+    );
+};
---
+++ src/data/pedidos.jsx
@@ -0,0 +1,41 @@
+export const pedidos = [
+    {   
+        id: "2",
+        title: "Don Quijote de la Mancha",
+        price: 12.50,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsRjryjximJfi6t09lPbdkpqZy_88ihqxCkA&s",
+        tracking: "75514951002",
+        state: "Preparación",
+        date: "30/04/2025"
+
+        
+    },
+    {
+        id: "5",
+        title: "1984",
+        price: 11.50,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsRjryjximJfi6t09lPbdkpqZy_88ihqxCkA&s",
+        tracking: "8021451025",
+        state: "Enviado",
+        date: "15/02/2025"
+    },
+    {
+        id: "10",
+        title: "Orgullo y prejuicio",
+        title2: "Ulises",
+        price: 22.98,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsRjryjximJfi6t09lPbdkpqZy_88ihqxCkA&s",
+        tracking: "8501783021",
+        state: "Entregado",
+        date: "30/01/2025"
+    },
+    {
+        id: "20",
+        title: "El conde de Montecristo",
+        price: 17.50,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsRjryjximJfi6t09lPbdkpqZy_88ihqxCkA&s",
+         tracking: "8981783021",
+         state: "Entregado",
+        date: "05/10/2024"
+    }
+];
