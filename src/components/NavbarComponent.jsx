f5424f4a041746f7144b7bd56c5ce60a6764a568
HaroldBarreraNino <hsbarreran@outlook.com>
2025-05-14 22:22:58 -0500

    Ajustes a los estilos

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import '../styles/Navbar.css';
 
 const NavBarComponent = () => {
   return (
-    // Los href deben apuntar a los paths que definieron en Routes en General.jsx para que funcionen
-    <nav className="bg-[#ba9c82] w-1/4 flex flex-col p-4 space-y-4 overflow-y-auto">
-      <a href="/" className="text-[#1b1f25] text-lg hover:bg-[#e0c3a4] p-2 rounded">
-        Comprar
+    <nav className="navBar">
+      <a href="/" className="navBar__link familia">
+        Inicio
       </a>
-      <a href="/productlist" className="text-[#1b1f25] text-lg hover:bg-[#e0c3a4] p-2 rounded">
+      <a href="/productlist" className="navBar__link familia">
         Lista de productos
       </a>
-      <a href="/support" className="text-[#1b1f25] text-lg hover:bg-[#e0c3a4] p-2 rounded">
+      <a href="/support" className="navBar__link familia">
         Soporte
       </a>
-      <a href="/myorders" className="text-[#1b1f25] text-lg hover:bg-[#e0c3a4] p-2 rounded">
+      <a href="/myorders" className="navBar__link familia">
         Mis Pedidos
       </a>
     </nav>
